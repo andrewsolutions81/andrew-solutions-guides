ad5f39faa84490926b859d50d1be28a7dd0833c5
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-12-12 15:50:25 -0500

    time conversion

--- exercises/3_plus_minus.js
+++ exercises/3_plus_minus.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /* https://www.hackerrank.com/challenges/one-month-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
 Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
 
--- exercises/4_time_conversion.js
+++ exercises/4_time_conversion.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /* https://www.hackerrank.com/challenges/one-month-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
 Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 
@@ -54,8 +55,22 @@ BlogScoringEnvironmentFAQAbout UsSupportCareersTerms Of ServicePrivacy Policy
  */
 
 function timeConversion(s) {
-// code here
+  // Extract the time components and AM/PM indicator
+  const hour = parseInt(s.substring(0, 2));
+  const minutes = s.substring(3, 5);
+  const seconds = s.substring(6, 8);
+  const amPm = s.substring(8);
 
+  // Convert to military time based on AM/PM indicator
+  let militaryHour = hour;
+  if (amPm === "PM" && hour !== 12) {
+    militaryHour += 12;
+  } else if (amPm === "AM" && hour === 12) {
+    militaryHour = 0;
+  }
+
+  // Combine the components and format as a string
+  return `${militaryHour.toString().padStart(2, "0")}:${minutes}:${seconds}`;
 }
 
 console.log(timeConversion("07:05:45PM"));//19:05:45
